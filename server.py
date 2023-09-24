b7ee1a61cd0eae0355b2412411f88846f6f34d05
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-24 12:34:29 +0000

    modify server.py in import

--- server.py
+++ server.py
@@ -1,28 +1,13 @@
-import os
-
-import requests
 from flask import Flask, Response, request
 
+from Controller.FunctionController import (
+    generate_answer,
+    message_parser,
+    send_message_telegram,
+    send_photo_telegram,
+)
+
 app = Flask(__name__)
-# Get BOT Token from telegram
-token = os.environ['TELEGRAM_KEY']
-
-
-# To Get Chat ID and message which is sent by client
-def message_parser(message):
-  chat_id = message['message']['chat']['id']
-  text = message['message']['text']
-  print("Chat ID: ", chat_id)
-  print("Message: ", text)
-  return chat_id, text
-
-
-# To send message using "SendMessage" API
-def send_message_telegram(chat_id, text):
-  url = f'https://api.telegram.org/bot{token}/sendMessage'
-  payload = {'chat_id': chat_id, 'text': text}
-  response = requests.post(url, json=payload)
-  return response
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -31,6 +16,7 @@ def index():
     msg = request.get_json()
     chat_id, incoming_que = message_parser(msg)
     answer = generate_answer(incoming_que)
+
     send_message_telegram(chat_id, answer)
     return Response('ok', status=200)
   else:
