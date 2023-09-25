0bb42494ad64291083f5ef185ce0164ac153b887
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-25 13:16:37 +0000

    modify definiton index

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from Controller.FunctionController import (
     generate_answer,
     message_parser,
     send_message_telegram,
-    send_photo_telegram,
 )
 
 app = Flask(__name__)
@@ -12,15 +11,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == 'POST':
-    msg = request.get_json()
-    chat_id, incoming_que = message_parser(msg)
-    answer = generate_answer(incoming_que)
-
-    if answer.startswith('http'):
-      send_photo_telegram(chat_id, answer)
+    if request.method == 'POST':
+        msg = request.get_json()
+        chat_id, incoming_que = message_parser(msg)
+        answer = generate_answer(incoming_que)
+        send_message_telegram(chat_id, answer)
+        return Response('ok', status=200)
     else:
-      send_message_telegram(chat_id, answer)
-    return Response('ok', status=200)
-  else:
-    return "<h1>Something went wrong</h1>"
+        return "<h1>Something went wrong</h1>"
