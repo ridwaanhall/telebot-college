f05cd20c3e795e6e330189f798e036c852515a8a
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-25 05:19:50 +0000

    Modify code

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from Controller.FunctionController import (
     generate_answer,
     message_parser,
     send_message_telegram,
+    send_photo_telegram,
 )
 
 app = Flask(__name__)
@@ -16,7 +17,10 @@ def index():
     chat_id, incoming_que = message_parser(msg)
     answer = generate_answer(incoming_que)
 
-    send_message_telegram(chat_id, answer)
+    if answer.startswith('http'):
+      send_photo_telegram(chat_id, answer)
+    else:
+      send_message_telegram(chat_id, answer)
     return Response('ok', status=200)
   else:
     return "<h1>Something went wrong</h1>"
